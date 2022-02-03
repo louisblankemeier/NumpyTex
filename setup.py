b6e5bd454f713c7cc5d4bfb918eb3f7fefd74a41
Louis Blankemeir <lblankem@torino.stanford.edu>
2022-02-03 12:49:31 -0800

    organized

--- setup.py
+++ setup.py
@@ -3,11 +3,6 @@ from setuptools import setup, find_packages
 setup(
     name='NumpyTex',
     version='0.0.1',    
-    description='Generates latex table scripts using a numpy array as input',
-    url='https://github.com/louisblankemeier/NumpyTex',
-    author='Louis Blankemeier',
-    author_email='lblankem@stanford.edu',
     install_requires=['numpy'],
-    include_package_data=True,
     packages=find_packages(),
 )
