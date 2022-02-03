faac19d7bd55d6b94589ac099e439ff283586ed4
Louis Blankemeir <lblankem@torino.stanford.edu>
2022-02-03 10:03:47 -0800

    setup added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='numpy_to_latex_table',
+    version='0.0.1',    
+    description='Generates latex table scripts using a numpy array as input',
+    url='https://github.com/shuds13/pyexample',
+    author='Louis Blankemeier',
+    author_email='lblankem@stanford.edu,
+    install_requires=['numpy'],
+)
