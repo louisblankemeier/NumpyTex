595fd8c7e78e1287c5e7e01dbab50719d3c07276
Louis Blankemeir <lblankem@torino.stanford.edu>
2022-02-03 12:00:52 -0800

    average and nan

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
 from setuptools import setup
 
 setup(
-    name='numpy_to_latex_table',
+    name='NumpyTex',
     version='0.0.1',    
     description='Generates latex table scripts using a numpy array as input',
-    url='https://github.com/louisblankemeier/numpy_to_latex_table',
+    url='https://github.com/louisblankemeier/NumpyTex',
     author='Louis Blankemeier',
     author_email='lblankem@stanford.edu',
     install_requires=['numpy'],
+    packages=find_packages('NumpyTex'),
 )
