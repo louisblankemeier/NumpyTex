8d6c08b75eab4a98dc69183b0f030d17e7d89ac7
Louis Blankemeir <lblankem@torino.stanford.edu>
2022-02-03 13:10:02 -0800

    organized

--- setup.py
+++ setup.py
@@ -3,6 +3,6 @@ from setuptools import setup, find_packages
 setup(
     name='NumpyTex',
     version='0.0.1',    
-    install_requires=['numpy', 'os'],
+    install_requires=['numpy'],
     packages=find_packages(),
 )
